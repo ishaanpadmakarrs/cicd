41d088c43ec91f9b66d77b5b13ed81b063197ff9
iprs <iprs@gmail.com>
2024-12-13 23:00:04 +0530

    unit test case updated

---
+++ test/test_operations.py
@@ -0,0 +1,10 @@
+from src.math_operations import add, subtract
+
+def test_add():
+    assert add(2,3)== 5
+    assert add(-3,-3)== -6
+
+
+def test_subtract():
+    assert subtract(2,3)== -1
+    assert subtract(-3,-3)== 0
